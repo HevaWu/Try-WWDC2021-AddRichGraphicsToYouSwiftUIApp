00b7586e5ce099e7a957337339953e76a8a2918d
He Wu(Heva) <hewu9375@gmail.com>
2021-06-10 22:39:25 +0900

    reset ParticleVisualizer

--- Shared/Views/Visualizers/ParticleVisualizer.swift
+++ Shared/Views/Visualizers/ParticleVisualizer.swift
@@ -12,26 +12,9 @@ struct ParticleVisualizer: View {
     @StateObject private var model = ParticleModel()
 
     var body: some View {
-        TimelineView(.animation) { timeline in
-            Canvas { context, size in
-                let now = timeline.date.timeIntervalSinceReferenceDate
-                model.update(time: now, size: size)
-
-                context.blendMode = .screen
-                model.forEachParticle { particle in
-                    var innerContext = context
-                    innerContext.opacity = particle.opacity
-                    innerContext.fill(
-                        Ellipse().path(in: particle.frame),
-                        with: particle.shading(gradients))
-                }
-            }
+        Canvas {
+            
         }
-        .gesture(
-            DragGesture(minimumDistance: 0)
-                .onEnded { model.add(position: $0.location) }
-        )
-        .accessibilityLabel("A particle visualizer")
     }
 }
 
