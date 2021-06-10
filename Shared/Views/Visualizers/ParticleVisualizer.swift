da6c485dda5bf048b10c5c1e421254662bc07e83
He Wu(Heva) <hewu9375@gmail.com>
2021-06-10 22:55:30 +0900

    add animation by using timeline to control image frame

--- Shared/Views/Visualizers/ParticleVisualizer.swift
+++ Shared/Views/Visualizers/ParticleVisualizer.swift
@@ -11,24 +11,31 @@ struct ParticleVisualizer: View {
     var gradients: [Gradient]
     
     var body: some View {
-        Canvas { context, size in
-            var image = context.resolve(Image(systemName: "sparkle"))
-            image.shading = .color(.blue)
-            let imageSize = image.size
-            
-            context.blendMode = .screen
-            for i in 0..<10 {
-                let frame = CGRect(
-                    x: 0.5 * size.width + Double(i) * imageSize.width,
-                    y: 0.5 * size.height,
-                    width: imageSize.width,
-                    height: imageSize.height
-                )
+        TimelineView(.animation) { timeline in
+            Canvas { context, size in
+                let now = timeline.date
+                    .timeIntervalSinceReferenceDate
+                let angle = Angle.degrees(now.remainder(dividingBy: 3) * 120)
+                let x = cos(angle.radians)
+                var image = context.resolve(Image(systemName: "sparkle"))
+                image.shading = .color(.blue)
+                let imageSize = image.size
                 
-                var innerContext = context
-                innerContext.opacity = 0.5
-                innerContext.fill(Ellipse().path(in: frame), with: .color(.cyan))
-                context.draw(image, in: frame)
+                // blendMode combine color
+                context.blendMode = .screen
+                for i in 0..<10 {
+                    let frame = CGRect(
+                        x: 0.5 * size.width + Double(i) * imageSize.width * x,
+                        y: 0.5 * size.height,
+                        width: imageSize.width,
+                        height: imageSize.height
+                    )
+                    
+                    var innerContext = context
+                    innerContext.opacity = 0.5
+                    innerContext.fill(Ellipse().path(in: frame), with: .color(.cyan))
+                    context.draw(image, in: frame)
+                }
             }
         }
     }
