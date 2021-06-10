4081fe21c5af683ce4e5947a2fe5f7ac910277d4
He Wu(Heva) <hewu9375@gmail.com>
2021-06-10 22:32:28 +0900

    try emoji

--- Shared/Views/GradientDetailView.swift
+++ Shared/Views/GradientDetailView.swift
@@ -43,7 +43,7 @@ struct GradientDetailView: View {
                 
                 Spacer()
                 
-                Text("\(gradient.stops.count) \(Text("colors").foregroundColor(.red))")
+                Text("\(gradient.stops.count) 🎉 \(Text("colors").foregroundColor(.red))")
                     .foregroundStyle(.secondary)
             }
             .padding()
